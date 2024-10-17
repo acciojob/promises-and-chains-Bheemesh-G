6f927bb68f086016811877eb91fa25834c96e131
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-10-17 20:58:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+document.getElementById('frm').addEventListener('submit',(event)=>{
+	event.preventDefault();
+	var age = parseInt(document.getElementById('age').value);
+	var name = document.getElementById('name').value;
+	consumePromise(name,age);
+})
+
+
+function getData(name,age)
+{
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			if(age>=18)
+			{
+				resolve(`Welcome, ${name}. You can vote.`)
+			}
+			else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000);
+	})
+}
+
+async function consumePromise(name,age)
+{
+	try{
+		var m = await getData(name,age);
+		alert(m);
+	}
+	catch(error)
+	{
+		alert(error);
+	}
+}
+
+
+
+
+
+
